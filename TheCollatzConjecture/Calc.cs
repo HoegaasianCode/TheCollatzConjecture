703bd36ac055d4f381a43cef50582706efdb71dc
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-17 16:50:19 +0100

    Initial commit

---
+++ TheCollatzConjecture/Calc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCollatzConjecture
+{
+    public class Calc
+    {
+        private int _int;
+
+        public Calc(int int1)
+        {
+            _int = int1;
+        }
+        
+        public int CollatzCalc()
+        {
+            int steps = 0;
+            while(_int != 1)
+            {
+                IsEven();
+                steps++;
+            }
+            return steps;
+        }
+
+        private void IsEven()
+        {
+            if (_int % 2 == 0) _int = _int / 2;
+            else _int = _int * 3 + 1;
+        }
+    }
+}
---
+++ TheCollatzConjecture/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TheCollatzConjecture
+{
+    class Program
+    {
+
+        // https://edabit.com/challenge/F6m5ZRyzK5fmqTrBG
+        // SOLVED
+
+        static void Main(string[] args)
+        {
+            var collatz = new Calc(10);
+            Console.Write(collatz.CollatzCalc());
+        }
+    }
+}
